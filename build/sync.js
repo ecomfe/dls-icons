aaff8e9f4ae700d059f610cfe660bb08732051dc
Justineo <justice360@gmail.com>
2023-02-23 20:19:24 +0800

    build: add sync script

---
+++ build/sync.js
@@ -0,0 +1,35 @@
+import https from 'https'
+
+const PACKAGES = [
+  'dls-icons-data',
+  'dls-icons-react',
+  'dls-icons-vue',
+  'dls-icons-vue-3'
+]
+
+async function syncPackage (name) {
+  const options = {
+    hostname: 'npmmirror.com',
+    path: `/sync/${name}`,
+    method: 'GET'
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.on('data', () => {})
+      res.on('end', () => {
+        resolve()
+      })
+    })
+    req.on('error', (e) => {
+      reject(e)
+    })
+    req.end()
+  })
+}
+
+Promise.all(PACKAGES.map(syncPackage)).then(() => {
+  console.log('Sync request sent for all packages.')
+}).catch((e) => {
+  console.error(e)
+})
